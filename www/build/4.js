1e1d328dc26766a46843c260f05066667389e8f7
Manuel Pino <mpino@atypax.com>
2019-01-13 21:11:42 -0500

    anado cambio

--- www/build/4.js
+++ www/build/4.js
@@ -8,11 +8,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LoginAfterPageModule", function() { return LoginAfterPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(18);
-<<<<<<< HEAD
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__login_after__ = __webpack_require__(131);
-=======
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__login_after__ = __webpack_require__(133);
->>>>>>> a2703c03f25a78e9dd368b6bc43134f6aa2a06ae
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
